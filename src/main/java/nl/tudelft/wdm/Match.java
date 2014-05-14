d76d29d6a6ecca6343fcc539612f2795dd1a87b9
Gijs Kuijer <gijs.kuijer@gmail.com>
2014-05-14 10:51:37 +0200

    added getters and setters to match

--- src/main/java/nl/tudelft/wdm/Match.java
+++ src/main/java/nl/tudelft/wdm/Match.java
@@ -3,7 +3,7 @@ package nl.tudelft.wdm;
 public class Match {
     // Original Map <PatternNode, Array<Match>> children; , but Array isn't a class in Java...
     // I guess Map <PatternNode, List<Match>> was meant, and Guava has a MultiMap that is abstracts Map<P, List<Q>>
-    private MutliMap<PatternNode, Match> children;
+    private MultiMap<PatternNode, Match> children;
     private TPEStack st;
     private int start;
     private STATUS status;
@@ -11,6 +11,42 @@ public class Match {
 
     public STATUS getStatus() {
         return status;
+    }
+
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+
+    public MultiMap<PatternNode, Match> getChildren() {
+        return children;
+    }
+
+    public void addChild(PatternNode pn, Match m) {
+        children.put(pn, m);
+    }
+
+    public TPEStack getSt() {
+        return st;
+    }
+
+    public void setSt(TPEStack st) {
+        this.st = st;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public Match getParent() {
+        return parent;
+    }
+
+    public void setParent(Match parent) {
+        this.parent = parent;
     }
 
     public enum STATUS {
